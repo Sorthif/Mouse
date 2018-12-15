4252e6ba54b68b426a430304ef95008c542c5ea5
philip914 <45289643+philip914@users.noreply.github.com>
2018-12-15 11:17:53 +0100

    Delete map.h

--- map.h
+++
@@ -1,8 +0,0 @@
-void readMAP(char* filename, char arr[MAPSIZE][MAPSIZE]);
-void printMapToFile(char* filename, char arr[MAPSIZE][MAPSIZE]);
-void printHashtags();
-void clearKorridor(char arr[MAPSIZE][MAPSIZE], int *x, int *y, int size);
-
-//Run this in main to generate a map.
-//Map will be stored in RandomMap.mp
-void makeMap(int x, int y, int size, int nCats, int nCheeses);
